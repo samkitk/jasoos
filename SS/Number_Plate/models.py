41b9087b47bcbf38272f35154b7083f0ce3fb7b7
Samkit <samkitkundalia@gmail.com>
2020-12-06 18:58:39 +0530

    Testing - changed model

--- SS/Number_Plate/models.py
+++ SS/Number_Plate/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from .imageProcess import OCR
 
-# Create your models here.
+class Photo(models.Model):
+
+    ocr = OCR()
+    input_image = models.ImageField()
+    number_identified = models.CharField(max_length=30)
+
+class Owner(models.Model):
+    name = models.CharField(max_length=50)
+    numberplate = models.CharField(max_length=30)
+    car_model = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    registration_year = models.IntegerField(max_length=4)
